10e4bfc06418680b8642b41f4d0130cfa5cdc293
hitman <hitman@assassin.brotherhood>
2023-05-18 00:41:11 +0530

    changed the number of workers to 0

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-uwsgi --socket :8080 --workers 1 --master --enable-threads --module core.wsgi
+uwsgi --socket :8080 --workers 0 --master --enable-threads --module core.wsgi
